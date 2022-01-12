91b0e64dd9820b4b83cb6d7f37a6294b37dc1203
John Whitten <johnwhitten.studio@gmail.com>
2022-01-11 21:03:22 -0800

    add js update html

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
-import $ from "jquery";
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import DrinkService from './js/drink-service.js';
+
+
+function clearFields(){
+  $('#search').val("");
+  $('#imageToShow').attr("");
+  $('.showTitle').text("");
+}
+
+$('#drinkSearch').click(function() {
+  let search = $('#search').val();
+  clearFields();
+  let promise = DrinkService.getSearch(search);
+  promise.then(function(response){
+    const body = JSON.parse(response);
+    let imageOfTheDay = body.drinks[0].strDrinkThumb;
+    $('#imageToShow').attr("src", imageOfTheDay);
+    let imageTitle = body.drinks[0].strDrink;
+    $('.showTitle').text(imageTitle);
+  });
+});
